1142becddc41d205bab0e5700e86ebc9c81f60cf
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2023-05-14 15:57:25 +0530

    Update 01_representation_line.py

--- Code for concept /Week 1/01_representation_line.py
+++ Code for concept /Week 1/01_representation_line.py
@@ -1,1 +1,14 @@
 print("Hello representation line")
+class Line:
+    def __init__(self, m, b):
+        self.m = m  # slope of the line
+        self.b = b  # y-intercept of the line
+
+    def __repr__(self):
+        return f"y = {self.m}x + {self.b}"
+
+# create a line object with slope = 2 and y-intercept = 3
+line = Line(2, 3)
+
+# print the line representation
+print(line)
